c4bb025f5e91f723f6edc178e45b37d8e102bbfe
Saikat Das <saikatdas0790@gmail.com>
2025-04-11 16:24:41 +0000

    Refactor flake.nix to create Python development shells for multiple versions

--- dev-shell/flake.nix
+++ dev-shell/flake.nix
@@ -2,18 +2,31 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
+      # A list of shell names and their Python versions
+      pythonVersions = {
+        python38 = pkgs.python38;
+        python39 = pkgs.python39;
+        python310 = pkgs.python310;
+        default = pkgs.python310;
+      };
+      # A function to make a shell with a python version
+      makePythonShell =
+        shellName: pythonPackage:
+        pkgs.mkShell {
+          # You could add extra packages you need here too
+          packages = [ pythonPackage ];
+          # You can also add commands that run on shell startup with shellHook
+          shellHook = ''
+            echo "Now entering ${shellName} environment."
+          '';
+        };
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = [
-          pkgs.cargo
-          pkgs.rustc
-          pkgs.python3
-        ];
-        RUST_LOG = 1;
-      };
+      # mapAttrs runs the given function (makePythonShell) against every value
+      # in the attribute set (pythonVersions) and returns a new set
+      devShells.x86_64-linux = builtins.mapAttrs makePythonShell pythonVersions;
     };
 }
