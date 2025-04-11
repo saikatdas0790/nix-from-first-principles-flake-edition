87fdbddc899e533f0e7c289deb01dbbd9051fe75
Saikat Das <saikatdas0790@gmail.com>
2025-04-11 16:19:49 +0000

    Add flake.nix and flake.lock for Nix development environment setup

---
+++ dev-shell/flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [
+          pkgs.cargo
+          pkgs.rustc
+          pkgs.python3
+        ];
+        RUST_LOG = 1;
+      };
+    };
+}
